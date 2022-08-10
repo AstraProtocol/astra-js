fc647d153f1ce3ac8637d0e8f9a6007026c722ac
Nguyễn Duy Anh <anh.nguyen10@tiki.vn>
2022-08-10 17:41:08 +0700

    feat(connector): Return error on empty

--- packages/my-connector/src/index.js
+++ packages/my-connector/src/index.js
@@ -2,22 +2,50 @@ import { checkContext } from './utils';
 import { AstraWalletConnector as MyAstraWalletConnector } from './my';
 import { AstraConnector  } from './mobile';
 
-
-
 export class AstraWalletConnector  {
+  constructor(){}
+
   static async create({ chainId, url }) {
-    const context = await checkContext(window);
-    if(context.isMy) {
-      const connector = new MyAstraWalletConnector ({
-        url,
-        chainId,
-      });
-      await connector.setup();
-      return connector;
+    try {
+      const context = await checkContext(window);
+      if(context.isMy) {
+        const connector = new MyAstraWalletConnector ({
+          url,
+          chainId,
+        });
+        await connector.setup();
+        return connector;
+      }
+      if(context.isAstra) {
+        return new AstraConnector({ url, chainId });
+      }
+    } catch (e) {
+      console.log(e);
     }
-    if(context.isAstra) {
-      return new AstraConnector({ url, chainId });
-    }
+    return new AstraWalletConnector();
+  }
+
+  async activate() {
+    const error = new Error('Not supported');
+    error.name = 'Astra Wallet Provider';
+    throw error;
+  }
+
+  async getProvider() {
     return null;
   }
+
+  async getChainId() {
+    return null;
+  }
+
+  async getAccount() {
+    return null;
+  }
+
+  deactivate() {
+    return;
+  }
+
+  changeChainId() {}
 }
