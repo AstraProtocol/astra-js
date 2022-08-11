2ca73458c5d6687e3f0ae8b1163d9cd45773ac29
Duc Nguyen <duc.nguyen@tiki.vn>
2022-08-11 13:40:10 +0700

    Update AstraConnector auto connect

--- packages/my-connector/src/mobile/index.js
+++ packages/my-connector/src/mobile/index.js
@@ -21,6 +21,15 @@ export class UserRejectedRequestError extends Error {
 
 export class AstraConnector extends AbstractConnector {
 
+  static async create({ chainId, url }) {
+    const connector = new AstraConnector({
+      url,
+      chainId,
+    });
+    await connector.setup();
+    return connector;
+  }
+
   constructor({ url, chainId }) {
     super({ supportedChainIds: [chainId] });
 
@@ -29,32 +38,32 @@ export class AstraConnector extends AbstractConnector {
     const mappers = {
       eth_chainId: chainId,
     };
-    
+    this.setupSuccess = false;
     this.myAstra = astra(window);
     this.provider = new MiniRpcProvider(chainId, url, mappers, this.myAstra);
   }
 
-
-  async activate() {
-    if (!this.myAstra) {
-      throw new NoAstraProviderError()
-    }
-    // try to activate + get account via eth_requestAccounts
-    let account
+  async setup() {
     try {
       await this.myAstra.enable(cosmosChainId(this.chainId))
       const key = await this.myAstra.getKey(cosmosChainId(this.chainId))
       const buffer = Buffer.from(key.address)
       this.provider.updateBechAddress(key.bech32Address)
-      account = `0x${buffer.toString('hex')}`
-    } catch (error) {
+      this.account = `0x${buffer.toString('hex')}`
+      this.setupSuccess = true;
+    } catch (e) {
       if (error.code === 4001) {
         throw new UserRejectedRequestError()
       }
       warning(false, 'eth_requestAccounts was unsuccessful, falling back to enable')
     }
+  }
 
-    return { provider: this.provider, chainId: this.chainId, account }
+  async activate() {
+    if (!this.myAstra || !this.setupSuccess) {
+      throw new NoAstraProviderError()
+    }
+    return { provider: this.provider, chainId: this.chainId, account: this.account };
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -68,24 +77,7 @@ export class AstraConnector extends AbstractConnector {
   }
 
   async getAccount() {
-    if (!this.provider) {
-      throw new NoAstraProviderError()
-    }
-    // try to activate + get account via eth_requestAccounts
-    let account
-    try {
-      await this.myAstra.enable(cosmosChainId(this.chainId))
-      const key = await this.myAstra.getKey(cosmosChainId(this.chainId))
-      this.provider.updateBechAddress(key.bech32Address)
-      const buffer = Buffer.from(key.address)
-      account = `0x${buffer.toString('hex')}`
-    } catch (error) {
-      if (error.code === 4001) {
-        throw new UserRejectedRequestError()
-      }
-      warning(false, 'eth_requestAccounts was unsuccessful, falling back to enable')
-    }
-    return account
+    return this.account
   }
 
   // eslint-disable-next-line class-methods-use-this, @typescript-eslint/no-empty-function
