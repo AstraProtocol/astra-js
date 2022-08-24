e96c0426ca2993ffd85f0e9be85c617debff73f0
vu.le <vu.le@tiki.vn>
2022-08-24 16:17:21 +0700

    update

--- packages/dugtrio/tests/wallet.test.js
+++ packages/dugtrio/tests/wallet.test.js
@@ -7,9 +7,9 @@ const storage = storageGenerator('data');
 const chainInfo = {
   name: 'Testnest',
   key: 'testnest',
-  chainId: 'astra_11112-1',
-  lcdUrl: 'https://api.astranaut.network',
-  rpcUrl: 'https://rpc.astranaut.network',
+  chainId: 'astra_11115-1',
+  lcdUrl: 'https://api.astranaut.dev',
+  rpcUrl: 'https://rpc.astranaut.dev',
   bech32Prefix: 'astra',
   gasLimit: '200000',
   gasPrice: '0.001aastra',
@@ -39,56 +39,36 @@ describe('Wallet', function () {
       const address = await provider.getAddress();
       expect(address).toEqual('astra12nnueg3904ukfjel4u695ma6tvrkqvqmrqstx6');
     });
-    test('send big number', async function () {
+    // test('send big number', async function () {
+    //   const tx = await provider.transfer(
+    //     'astra19u6ft0g0zldkdewd8t76s2tftzpezly7gx7x7h',
+    //     1000000,
+    //     'Unit test by Duy Anh'
+    //   );
+    //   console.log({tx})
+    //   expect(tx).toHaveProperty('txHash');
+    //   expect(tx).toHaveProperty('tx');
+    // });
+    test('send decimal number', async function () {
       const tx = await provider.transfer(
         'astra19u6ft0g0zldkdewd8t76s2tftzpezly7gx7x7h',
-        1000000,
+        1.1,
         'Unit test by Duy Anh'
       );
+      console.log({tx})
       expect(tx).toHaveProperty('txHash');
       expect(tx).toHaveProperty('tx');
     });
-    test('claim reward', async function () {
-      const tx = await provider.withdrawDelegatorReward(
-        'astravaloper1zy8cmnacuxl7sh0jkyunzfrz4dfmjc7h8e6wm4',
-        'Unit test by Duy Anh'
-      );
+    // test('claim reward', async function () {
+    //   const tx = await provider.withdrawDelegatorReward(
+    //     'astravaloper1zy8cmnacuxl7sh0jkyunzfrz4dfmjc7h8e6wm4',
+    //     'Unit test by Duy Anh'
+    //   );
 
-      expect(tx).toBeTruthy();
-      expect(tx).toHaveProperty('txHash');
-      expect(tx).toHaveProperty('logs', []);
-      expect(tx).toHaveProperty('tx');
-    });
-    // test('connect', async function (done) {
-    //   console.log(provider.getAccount());
-    //   const WC_URI =
-    //     'wc:98d54a45a3144774250b8aa579b64e8f513641e719dc30f0c7442985a5f2bba9@2?relay-protocol=waku&symKey=11c098ef0fd15b8d09150c5b3d0667fcb357529586a96330a26f4ca709c68677';
-
-    //   await provider.initSignClient({
-    //       projectId: 'd34cb9f3c11ab0a7c8d8ee9275147785',
-    //       relayUrl: 'wss://relay.astranaut.network',
-    //       metadata: {
-    //         name: 'Test Wallet',
-    //         description: 'React Wallet for WalletConnect',
-    //         url: 'https://walletconnect.com/',
-    //         icons: ['https://avatars.githubusercontent.com/u/37784886'],
-    //       }
-    //   });
-
-    //   const client = provider.getSignClient();
-    //   console.log({client})
-    //   client.onSessionProposal(async (data) => {
-    //     console.log('proposal');
-    //     await client.approveProposal(data, provider.getAddress());
-    //     console.log('approve ok');
-    //   });
-    //   client.onSessionRequest(async (data) => {
-    //     console.log('onSessionRequest', data)
-    //     await client.approveRequest(data, provider.getAccount());
-    //     console.log('approve request ok');
-    //     done();
-    //   });
-    //   await client.pair({ uri: WC_URI });
-    // }, 100000);
+    //   expect(tx).toBeTruthy();
+    //   expect(tx).toHaveProperty('txHash');
+    //   expect(tx).toHaveProperty('logs', []);
+    //   expect(tx).toHaveProperty('tx');
+    // });
   });
 });
--- packages/tx/src/tx-helper.js
+++ packages/tx/src/tx-helper.js
@@ -1,6 +1,4 @@
-import { coins } from '@cosmjs/proto-signing';
-import { Uint64 } from '@cosmjs/math';
-
+import { Dec, DecUtils } from "@keplr-wallet/unit";
 export const genSignDoc = (amount, currency, recipient, memo, stdFee, signOptions) => ({
   amount,
   currency,
@@ -11,11 +9,9 @@ export const genSignDoc = (amount, currency, recipient, memo, stdFee, signOption
 });
 
 export const actualAmount = (amount, coinDecimals) => {
-  // const _amount = Uint64.fromNumber(Number(amount));
-  // const _coinDecimals = Uint64.fromNumber(Number(coinDecimals));
-  // const tenNumber = Uint64.fromNumber(10);
-  // return _amount.data.mul(tenNumber.data.pow(_coinDecimals.data)).toString();
-  return String(Number(amount) * 10 ** Number(coinDecimals))
+  const dec = new Dec(amount);
+  const dec2 = dec.mul(DecUtils.getPrecisionDec(coinDecimals));
+  return dec2.truncate().toString();
 };
 
 export const genMessage = (type) => (address) => (__inputData) => {
@@ -24,12 +20,16 @@ export const genMessage = (type) => (address) => (__inputData) => {
     currency: { coinDecimals, coinMinimalDenom },
     amount,
   } = __inputData;
+
   return {
     type,
     value: {
       from_address: address,
       to_address: recipient,
-      amount: coins(actualAmount(amount, coinDecimals), coinMinimalDenom),
+      amount: [{
+        amount: actualAmount(amount, coinDecimals),
+        denom: coinMinimalDenom
+      }]
     },
   };
 };
