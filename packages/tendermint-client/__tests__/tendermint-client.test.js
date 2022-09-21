81c1b860ddbfb2ef4944042617cdec54cdeb1d97
Nguyễn Duy Anh <anh.nguyen10@tiki.vn>
2022-09-21 14:50:00 +0700

    remove dev test

--- packages/tendermint-client/__tests__/tendermint-client.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const tendermintClient = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(tendermintClient(), 'Hello from tendermintClient');
-console.info("tendermintClient tests passed");
