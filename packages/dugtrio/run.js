3451cb8230267255d65828cf21f5f342369b69eb
vu.le <vu.le@tiki.vn>
2022-11-14 15:22:18 +0700

    Delete run.js

--- packages/dugtrio/run.js
+++
@@ -1,7 +0,0 @@
-const {BigNumber} = require('@ethersproject/bignumber');
-// const {Dec, Int} = require('@keplr-wallet/unit');
-
-// const a = new Dec('11.1').mul((new Dec('10')).pow(new Int('18')));
-// console.log(BigNumber.from(a.toString(0)))
-
-console.log(BigNumber.from('0x1423').toString())
